1a7b8dc9d56435dd7391031ab33f129475b3c2c3
Alejandro Fernández <alejandrofernandezruiz8@gmail.com>
2019-07-28 18:03:52 +0200

    First commit. Project creation.

---
+++ WeatherAppv2/WeatherAppv2/WeatherAppv2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppv2
+//
+//  Created by Alejandro Fernández Ruiz on 28/07/2019.
+//  Copyright © 2019 Alejandro Fernández Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherAppv2/WeatherAppv2/WeatherAppv2Tests/WeatherAppv2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherAppv2Tests.swift
+//  WeatherAppv2Tests
+//
+//  Created by Alejandro Fernández Ruiz on 28/07/2019.
+//  Copyright © 2019 Alejandro Fernández Ruiz. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherAppv2
+
+class WeatherAppv2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
